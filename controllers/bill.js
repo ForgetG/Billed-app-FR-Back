3d25c47a0124bc05d43e1ae717a894d5d7825926
Forget Guillaume <g.forget1999@gmail.com>
2025-03-25 15:40:51 +0100

    change type of image allowed

--- controllers/bill.js
+++ controllers/bill.js
@@ -2,7 +2,7 @@ const { Bill } = require('../models');
 
 const getFileURL = (filePath) => `http://localhost:5678/${filePath}`;
 
-const isPicture = (mimeType) => ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'].includes(mimeType);
+const isPicture = (mimeType) => ['image/jpeg', 'image/jpg', 'image/png'].includes(mimeType);
 
 const create = async (req, res) => {
   const { user } = req;
